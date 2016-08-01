4134310f22322e7a837421c01223d37c3738651e
patrickcberry <patrickcberry@gmail.com>
2016-08-01 15:34:25 +1000

    General update

--- keygen.sh
+++ keygen.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-echo 'Set up security'
+echo 'Generate security keys'
 echo '----------------------------------'
 echo 'usage sh keygen.sh output-file-name'
 
 # Generate the key pair
 #
-# -t rsa
-# -b 4096
-# -v
-# -f $1
-# -N ""
+# -t rsa	RSA key type
+# -b 4096	Number of bits in the key
+# -v		Verbose output
+# -f $1		Key output file
+# -N ""		Pass-phrase
 
 ssh-keygen -t rsa -b 4096 -v -f $1 -N ""
 
